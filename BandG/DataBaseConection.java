eab338e8290212a7cb194562463493d2e9d9e335
Bruno Campos <bru.lacerda.c@gmail.com>
2025-03-25 01:55:51 -0300

    conector com banco de dados melhorados

--- BandG/DataBaseConection.java
+++ BandG/DataBaseConection.java
@@ -4,41 +4,43 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class DataBaseConection {
     public DataBaseConection(){};
     
     private static final String URL = "jdbc:mysql://localhost:3306/b&g";
-    private static final String userName = "Bruno";
-    private static final String password = "123";
     
-    
-    public static void main(String[] args){
-        String team = "Defalt";
-        String[] members = {"Empth", "Empth", "Empth"};
-        String membersList = "Empth";
-        Float lapOne = 0f;
-        Float lapTwo = 0f;
-        Float avaregeTime = (lapOne + lapTwo) / 2;
+    public static Connection Connect(){
+        Connection cn = null;
         
         try{
-            Connection connection = DriverManager.getConnection(URL, userName, password);
+            Class.forName("com.mysql.jdbc.Driver");
+            cn = DriverManager.getConnection(URL, "root", "");
+            
             String sql = "INSERT INTO lapcounter(TEAM, MEMBERS, LAP_ONE, LAP_TWO, AVAREGE_TIME) VALUES(?, ?, ?, ?, ?)";
             
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(0, team);
-            preparedStatement.setString(1, membersList);
-            preparedStatement.setFloat(2, lapOne);
-            preparedStatement.setFloat(3, lapTwo);
-            preparedStatement.setFloat(4, avaregeTime);
-            
+            PreparedStatement preparedStatement = cn.prepareStatement(sql);
+            preparedStatement.setString(1, "B and G");
+            preparedStatement.setString(2, "Bruno e Gabriela");
+            preparedStatement.setFloat(3, 12.3f);
+            preparedStatement.setFloat(4, 11.8f);
+            preparedStatement.setFloat(5, (12.3f + 11.8f) / 2);      
             int rowAffected = preparedStatement.executeUpdate();
             
-            preparedStatement.close();
-            connection.close();
-        } catch(SQLException e){
-            e.printStackTrace();
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "SQLException: " + e);
+            
+        }catch(ClassNotFoundException ex){
+            JOptionPane.showMessageDialog(null, "Class not found: " + ex);
+            
         }
+        return cn;
+    }
+    
+    public static void main(String[] args){
+        Connect();
     }
 }
